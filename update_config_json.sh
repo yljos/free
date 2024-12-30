4fc9ac3b4b478b0b0fe28d9f90302744012177f6
huai12138 <huaihuaidehuaihuai@gmail.com>
2024-12-30 16:08:11 +0800

    sinbox

--- update_config_json.sh
+++ update_config_json.sh
@@ -1,4 +1,5 @@
 #！/bin/sh
-curl -o /etc/sing-box/config.json http://nas:5000/config/https://\&app\=sing_box\&file\=2
+curl -o /etc/sing-box/config.json http://nas:5000/config/https://app.mitce.com/\?sid\=\&token\=\&emoji=0\&ua\=sing-box\&file\=2
 cd /usr/share/sing-box && rm -rf ui
 /etc/init.d/sing-box restart
+echo "sing-box config.json updated"
