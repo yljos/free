3ae8a75370391d6496a8710d7b61806fcef19909
huai12138 <huaihuaidehuaihuai@gmail.com>
2024-12-30 11:53:42 +0800

    update

--- update_sing_box.sh
+++ update_sing_box.sh
@@ -1,1 +1,9 @@
-cd /usr/bin && curl -LO $(curl -s https://api.github.com/repos/SagerNet/sing-box/releases/latest | grep "browser_download_url.*linux-amd64" | cut -d '"' -f 4) && tar zxvf *.tar.gz --strip-components 1 && chown root:root sing-box && rm LICENSE && rm *.tar.gz
+#!/bin/sh
+cd /usr/bin && \
+curl -LO $(curl -s https://api.github.com/repos/SagerNet/sing-box/releases | \
+grep "browser_download_url.*linux-amd64" | head -n 1 | cut -d '"' -f 4) && \
+tar zxvf *.tar.gz --strip-components=1 && \
+chown root:root sing-box && \
+chmod +x sing-box && \
+rm LICENSE && rm *.tar.gz
+/etc/init.d/sing-box restart
