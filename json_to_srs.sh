cc668fd487f6b34bc140470ae65758d721b96d25
huai12138 <huaihuaidehuaihuai@gmail.com>
2025-01-16 20:07:29 +0800

    fix

---
+++ json_to_srs.sh
@@ -0,0 +1,5 @@
+./sing-box rule-set compile proxy.json
+./sing-box rule-set compile direct.json
+./sing-box rule-set compile pass-ip.json
+./sing-box rule-set compile direct-ip.json
+./sing-box rule-set compile pikpak-download.json
