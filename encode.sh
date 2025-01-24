8241b9ac98cd606b28815987857973ad5e0c0549
huai12138 <huaihuaidehuaihuai@gmail.com>
2025-01-24 23:12:30 +0800

    fix

--- encode.sh
+++ encode.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cat 0 > temp.txt
+cat 0.txt > temp.txt
 cat temp.txt | python -c "import sys, urllib.parse; print(urllib.parse.quote(sys.stdin.read().strip()))" > singbox.txt
 # 使用 tr 命令移除可能的回车符，并使用 echo -n 避免添加换行符
 echo -n "&app=clashmeta" | tr -d '\r' >> temp.txt 
