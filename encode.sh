52c3d4ebe2df8ceb52f9ccce58cc8f2e9ded7319
huai12138 <huaihuaidehuaihuai@gmail.com>
2025-01-26 00:25:31 +0800

    fix

--- encode.sh
+++ encode.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 cat 0.txt > temp.txt
-cat temp.txt | python -c "import sys, urllib.parse; print(urllib.parse.quote(sys.stdin.read().strip()))" > singbox.txt
+echo -n "https://singbox.suckless.top:8443/" > singbox.txt
+cat temp.txt | python -c "import sys, urllib.parse; print(urllib.parse.quote(sys.stdin.read().strip()))" >> singbox.txt
 # 使用 tr 命令移除可能的回车符，并使用 echo -n 避免添加换行符
-echo -n "&app=clashmeta" | tr -d '\r' >> temp.txt 
+# echo -n "&app=clashmeta" | tr -d '\r' >> temp.txt 
 # 使用strip()确保移除所有额外的空白字符
 cat temp.txt | python -c "import sys, urllib.parse; print(urllib.parse.quote(sys.stdin.read().strip()))" > temp2.txt
 # 添加基础URL并与编码后的内容合并
