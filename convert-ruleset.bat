8c9f92da3353c5b5d3e4c69a50a5760a4752a5a5
yljos <yljos@outlook.com>
2025-07-21 22:15:54 +0800

    fix

--- convert-ruleset.bat
+++ convert-ruleset.bat
@@ -16,7 +16,7 @@ echo === Converting YAML to MRS format ===
 .\mihomo.exe convert-ruleset domain yaml proxy.yaml proxy.mrs
 .\mihomo.exe convert-ruleset domain yaml direct.yaml direct.mrs
 .\mihomo.exe convert-ruleset ipcidr yaml direct-ip.yaml direct-ip.mrs
-.\mihomo.exe convert-ruleset domain yaml direct.yaml direct-cf.mrs
+.\mihomo.exe convert-ruleset domain yaml direct-cf.yaml direct-cf.mrs
 
 echo YAML files conversion completed
 
